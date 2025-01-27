a75fee3b2ea89ba09f9acf788554bb75f6731331
Ali Hassan <AliHassan0030147@>
2025-01-27 22:55:25 +0000

    Change this Program into Palindrome Program

--- Hassan.py
+++ Hassan.py
@@ -1,2 +1,23 @@
-print("HEllo")
-print("Hello Pakistan")
+x=input("Enter a number:")
+l=len(x)
+n=False
+if x[-1]== x[0]:
+    if len(x)%2==1:
+               y=0
+               while len(x)/2>y:
+                   if x[y]==x[l-1]:
+                       n=True
+                   else:
+                       n=False
+                       break
+                   y+=1
+                   l-=1
+else:
+    print("The ", x, " is not Palindrome")
+if n==1:
+    print("The ",x," is Palindrome")
+else:
+    print("The ",x," is not Palindrome")
+
+
+
